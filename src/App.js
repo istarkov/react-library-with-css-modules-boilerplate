3893fce30feec7ec7542f278a82d7be86b1809fb
cybice <istarkov@gmail.com>
2015-12-11 20:31:39 +0300

    Fix preset order

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import Func from './func';
+
 export default class App extends Component {
+  tmp_ = 101;
+
   render() {
     return (
+      <div>
+      {this.tmp_}
       <Func />
+      </div>
     );
   }
 }
